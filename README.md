1567ecc40d5516b82b71805a4a83fe4e3b30cec9
Susant Paudel <techbizznepal@gmail.com>
2023-11-26 16:42:26 +0545

    v0.1:deployment enviroment setup

---
+++ README.md
@@ -0,0 +1,21 @@
+## Create folder for {project} and clone this repo
+
+```
+git clone git@github.com:techbizz-nepal/deployment.git .
+```
+
+## Inside Project root run these steps to start the project
+
+### give permission to run the script
+
+```
+chmod +x ./deployment/prepare_context.sh
+```
+
+### build enviroment
+
+```
+./deployment/prepare_context.sh build
+```
+
+# After setting up environment you can start your project in src folder 
---
+++ prepare_context.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
+#imports
+. $SCRIPT_DIR/variables.h
+. $SCRIPT_DIR/mysql.h
+. $SCRIPT_DIR/docker.h
+
+
+if [ "$FIRST_ARG" = "build" ]; then
+  create_sql_file_if_not_exist "$MYSQL_ENTRY_POINT_CREATE_MAIN_DB_CONTENT" "$MAIN_DB_FILE_PATH"
+  create_sql_file_if_not_exist "$MYSQL_ENTRY_POINT_CREATE_TEST_DB_CONTENT" "$TEST_DB_FILE_PATH"
+  docker_build
+  docker_up
+fi
+
+if [ "$FIRST_ARG" = "up" ]; then
+  docker_up
+fi
+
+if [ "$FIRST_ARG" = "down" ]; then
+  docker_down
+fi
