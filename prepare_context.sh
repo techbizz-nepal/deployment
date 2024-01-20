e5ff6efee546a3eae95957e69851875c9a14d5e5
Susant Paudel <techbizznepal@gmail.com>
2024-01-20 20:52:23 +0545

    refactor

--- prepare_context.sh
+++ prepare_context.sh
@@ -5,9 +5,11 @@ SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
 . $SCRIPT_DIR/mysql.h
 . $SCRIPT_DIR/docker.h
 
+if ! [ -f "$SCRIPT_DIR/.env" ]; then
+  cp $SCRIPT_DIR/.env.example $SCRIPT_DIR/.env
+fi
 
 if [ "$FIRST_ARG" = "build" ]; then
-  cp $SCRIPT_DIR/.env.example $SCRIPT_DIR/.env
   create_sql_file_if_not_exist "$MYSQL_ENTRY_POINT_CREATE_MAIN_DB_CONTENT" "$MAIN_DB_FILE_PATH"
   create_sql_file_if_not_exist "$MYSQL_ENTRY_POINT_CREATE_TEST_DB_CONTENT" "$TEST_DB_FILE_PATH"
   docker_build
